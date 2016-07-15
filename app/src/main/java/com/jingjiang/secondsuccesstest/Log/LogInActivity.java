2e9014337dc130e8dd11816e7e11fb440d7a4f46
关玉鑫 <2548197052@qq.com>
2016-07-15 17:45:49 +0800

    jialeyigebao

---
+++ app/src/main/java/com/jingjiang/secondsuccesstest/Log/LogInActivity.java
@@ -0,0 +1,11 @@
+package com.jingjiang.secondsuccesstest.Log;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/7/15.
+ */
+public class LogInActivity extends AppCompatActivity {
+    ///aa
+    
+}
