e5d208c62ae5d9671905bc8f565ff2011515eec6
关玉鑫 <2548197052@qq.com>
2016-07-16 16:23:03 +0800

    该了背景  加了一个包一个退出Activity

---
+++ app/src/main/java/com/jingjiang/secondsuccesstest/Log/signout/SignOutActivity.java
@@ -0,0 +1,10 @@
+package com.jingjiang.secondsuccesstest.Log.signout;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by guan on 16/7/16.
+ * 退出
+ */
+public class SignOutActivity extends AppCompatActivity {
+}
