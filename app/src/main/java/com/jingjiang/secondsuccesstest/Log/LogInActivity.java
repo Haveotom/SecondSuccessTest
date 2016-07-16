80e1dc5019af0217ca85f7f21342c4a7f215af2e
关玉鑫 <2548197052@qq.com>
2016-07-16 16:01:09 +0800

    加了一条注释

--- app/src/main/java/com/jingjiang/secondsuccesstest/Log/LogInActivity.java
+++ app/src/main/java/com/jingjiang/secondsuccesstest/Log/LogInActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
  * Created by dllo on 16/7/15.
  */
 public class LogInActivity extends AppCompatActivity {
-    ///aa
+    //登陆 7.1 5
+    //注册 7. 16
     
 }
