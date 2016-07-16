bab961492a84a54f23ec29e46f55255b1c7d597a
Haveotom <746232495@qq.com>
2016-07-16 16:23:54 +0800

    提交完成的类

---
+++ app/src/main/java/com/jingjiang/secondsuccesstest/net/Net.java
@@ -0,0 +1,8 @@
+package com.jingjiang.secondsuccesstest.net;
+
+/**
+ * Created by dllo on 16/7/16.
+ * 璟江
+ */
+public class Net {
+}
---
+++ app/src/main/java/com/jingjiang/secondsuccesstest/tool/Tool.java
@@ -0,0 +1,8 @@
+package com.jingjiang.secondsuccesstest.tool;
+
+/**
+ * Created by dllo on 16/7/16.
+ * 璟江
+ */
+public class Tool {
+}
